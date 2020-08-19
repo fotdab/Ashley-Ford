825308055be8a5e7de678b7d49ab30f08706f230
fotdab <69926622+fotdab@users.noreply.github.com>
2020-08-19 16:31:55 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**************************************
+* Java Program Name
+****************************************
+* Student Name (first and last)
+* Date
+* Course Number and Section
+***************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Ashley Ford>");
+	System.out.println("<lab 1>");
+	System.out.println("<Introduction to the Course>");
+	System.out.println("<8/19/2020>");
+	System.out.println("<255-C90>");
+	}
+}
+
